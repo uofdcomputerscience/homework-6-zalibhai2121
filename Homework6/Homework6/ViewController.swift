bf07efa5d913c5616ead722408051fc3a3ad51c1
Russell Mirabelli <russell@mirabellifamily.org>
2019-10-27 13:16:54 -0500

    Initial commit

---
+++ Homework6/Homework6/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Homework6
+//
+//  Created by Russell Mirabelli on 10/26/19.
+//  Copyright © 2019 Russell Mirabelli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var segmentedController: UISegmentedControl!
+    @IBOutlet var selectionSwitch: UISwitch!
+    @IBOutlet var selectionButton: UIButton!
+    @IBOutlet var continueButton: UIButton!
+    
+    @IBAction func segmentedControllerDidChangeValue(_ sender: UISegmentedControl) {
+    }
+    
+    @IBAction func selectionSwitchDidChangeValue(_ sender: UISwitch) {
+    }
+    
+    @IBAction func selectionButtonTapped(_ sender: UIButton) {
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,10 @@
+# uofdmobile_homework6
+U of D Mobile Development Homework 6
+
+In order to prepare for project 2, this project asks you to perform a few simple tasks:
+
+* Check if a switch has been switched on
+* Check if a button has been selected
+* Check the value of a selector
+
+If the selector's value is 2, and the switch is switched on, and the button has been set to "selected" mode, enable the final button on the screen to perform a final transition.
