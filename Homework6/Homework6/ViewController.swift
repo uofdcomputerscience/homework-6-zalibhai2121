799884328328291e32e85a836ae85707dd1f1ef0
Zainab <zalibhai2121@gmail.com>
2019-10-29 17:23:39 -0500

    finish

--- Homework6/Homework6/ViewController.swift
+++ Homework6/Homework6/ViewController.swift
@@ -16,12 +16,34 @@ class ViewController: UIViewController {
     @IBOutlet var continueButton: UIButton!
     
     @IBAction func segmentedControllerDidChangeValue(_ sender: UISegmentedControl) {
+        switch segmentedController.selectedSegmentIndex {
+        case 0:
+            selectionSwitch.isHidden = false
+            selectionSwitch?.isOn = false
+            continueButton.isEnabled = false
+        case 1:
+            selectionSwitch.isHidden = false
+            selectionSwitch?.isOn = true
+            continueButton.isEnabled = false
+        default:
+            selectionSwitch?.isOn = false
+            continueButton.isEnabled = false
+        }
+
     }
     
     @IBAction func selectionSwitchDidChangeValue(_ sender: UISwitch) {
+        selectionButton.isEnabled = false
+        if selectionSwitch?.isOn == true {
+            selectionButton.isEnabled = true
+        }
     }
     
     @IBAction func selectionButtonTapped(_ sender: UIButton) {
+        continueButton.isEnabled = true
+        if selectionButton?.isSelected == true {
+            continueButton.isEnabled = false
+        }
     }
 }
 
